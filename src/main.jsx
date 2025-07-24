12f470f95f52967176049f0bc0f6fcd15e8038bc
Sai Karthik <sangarusaikarthik19@gmail.com>
2025-07-24 13:30:09 +0530

    Fix for Vercel routing and base path

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { CartProvider } from "./stores/context/CartContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <React.StrictMode>
       <CartProvider>
         <App />
       </CartProvider>
     </React.StrictMode>
-  </HashRouter>
+    ,
+  </BrowserRouter>
 );
