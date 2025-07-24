bde9f81c8aca20546c4a3c4cd5b33c1310c5a03e
Sai Karthik <sangarusaikarthik19@gmail.com>
2025-07-24 13:23:38 +0530

    Fix routing and config for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { CartProvider } from './stores/context/CartContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
+import { CartProvider } from "./stores/context/CartContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<BrowserRouter>
-  <React.StrictMode>
-    <CartProvider>
-    <App />
-    </CartProvider>
-  </React.StrictMode>,
-</BrowserRouter>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    <React.StrictMode>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </React.StrictMode>
+  </HashRouter>
+);
